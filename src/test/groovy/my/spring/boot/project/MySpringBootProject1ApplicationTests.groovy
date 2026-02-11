577a3318eeaf434887f60554022bee6aa3b2c2bc
esperithm16 <rajatshaily17@gmail.com>
2026-02-11 21:54:32 +0530

    Initial commit

---
+++ src/test/groovy/my/spring/boot/project/MySpringBootProject1ApplicationTests.groovy
@@ -0,0 +1,13 @@
+package my.spring.boot.project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MySpringBootProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
