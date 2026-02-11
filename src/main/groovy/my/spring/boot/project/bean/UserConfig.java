5501cd5f41cf98700158be0f43c1d830fb041729
esperithm16 <rajatshaily17@gmail.com>
2026-02-11 22:06:43 +0530

    UserCOnfig

--- src/main/groovy/my/spring/boot/project/bean/UserConfig.java
+++ src/main/groovy/my/spring/boot/project/bean/UserConfig.java
@@ -1,4 +1,13 @@
 package my.spring.boot.project.bean;
 
 public class UserConfig {
+    private String name;
+    private String ClassName;
+    public  void setName(final String name){
+        this.name=name;
+    }
+
+    public void setClassName(String className) {
+        this.ClassName=ClassName;
+    }
 }
