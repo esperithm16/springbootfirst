5622ab928a832a4652cba5879ac801c420d8f6e4
esperithm16 <rajatshaily17@gmail.com>
2026-02-17 19:59:04 +0530

    remove xml

--- src/main/groovy/my/spring/boot/project/App.groovy
+++ src/main/groovy/my/spring/boot/project/App.groovy
@@ -6,20 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.ApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext
 
+
 @SpringBootApplication
 class App {
-
     static void main(String[] args) {
-
-        // Spring Boot context
-        SpringApplication.run(App.class, args)
-
-        // XML context (same as screenshot)
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext.xml")
-
-        UserConfig config = (UserConfig) context.getBean("userConfigBean")
-
-
+        SpringApplication.run(App, args)
     }
 }
